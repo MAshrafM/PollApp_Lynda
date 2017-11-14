8ca4a4c7d675e0c2a4aa37bb4ac43b014d5dbd27
MAshraf <ee.mashraf@gmail.com>
2017-11-14 06:00:20 +0200

    react es6

---
+++ components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class app extends Component({
+  render() {
+    return (
+      <h1>
+        Hello world from React
+      </h1>
+    )
+  }
+})
+
+export default app
---
+++ public/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import app from '../components/app'
+
+React.render(<app />, document.getElementById('react-container'))
