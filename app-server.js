14a0d29587e68f93ffff384f38914195d6321cd4
MAshraf <ee.mashraf@gmail.com>
2017-11-15 06:09:06 +0200

    3.1 socket.io

--- app-server.js
+++ app-server.js
@@ -5,6 +5,11 @@ const app = express()
 app.use(express.static('./public'))
 app.use(express.static('./node_modules/bootstrap/dist'))
 
-app.listen(3000)
+const server = app.listen(3000)
+const io = require('socket.io').listen(server)
+
+io.sockets.on('connection', socket => {
+  console.log("Connected: %s", socket.id)
+})
 
 console.log('Server is running at localhost')
--- components/app.js
+++ components/app.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import io from 'socket.io-client'
 
 export class App extends React.Component {
+  componentWillMount () {
+    this.socket = io('http://localhost:3000')
+    this.socket.on('connect', this.connect)
+  }
+  
+  connect () {
+    console.log('Connected: '+ this.socket.id)
+  }
+  
   render(){
     return (
       <h1>
